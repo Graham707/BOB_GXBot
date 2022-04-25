6b11da7e0fcd4d4a13adc1d8b90ccb5f64ce7d1b
Graham707 <103318711+Graham707@users.noreply.github.com>
2022-04-25 12:57:18 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,27 +7,27 @@ class LuciferMoringstar(object):
     ABOUT_MSG = """
 🤖 My Name [{bot_name}](t.me/{bot_username})
 
-🧑 My Dev : [PR0FESS0R-99](t.me/PR0FESS0R_99)
+🧑 My Dev : [TheUltimateCat](t.me/)
 
 🧑 My Creator : {dev_name}
 
-📦 My Source : [Click Here](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+🖇️ My Language : [Python3](https://youtu.be/Af055Eozk9s
 
-📺 My Tutorial : [Ckick Here](https://youtu.be/FCU_XJmyG_U)
+🏷️ My Updates : [GX Botz](t.me/GX_Botz)
 
-🗃️ My Library : [Pyrogram](https://youtu.be/Af055Eozk9s)
+🗣️ My Support : [GX Bot Support](t.me/GXBot_Support)"""
 
-🖇️ My Language : [Python3](https://youtu.be/Af055Eozk9s)
 
-🗃️ My Database : [Mongodb](https://youtu.be/gBLTsH-IXr0)
 
-🗳️ My Server : [Heroku](https://youtu.be/xcZD55td6Vw)
 
-🏷️ My Updates : [Mo Tech](t.me/Mo_Tech_YT)
 
-🗣️ My Support : [Any Doubt](t.me/Mo_Tech_Group)"""
 
-    FILE_CAPTIONS = """Hello 👋 {mention}\n\n📁Title {title}\n\n🔘Size {size}"""
+
+
+
+
+
+    FILE_CAPTIONS = """Hello 👋 {mention}\n\n📁Title <code>{title}</code>n\n🔘Size {size}"""
 
     PR0FESS0R_99 = """
 **ADMINS COMMANDS**
